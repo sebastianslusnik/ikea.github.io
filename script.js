dd72f3a4e291cc07d30b6e4bdaedc5d0f9b6a3b7
Sebastián Slušník <46315566+sebastianslusnik@users.noreply.github.com>
2025-02-17 18:31:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 // Sebastián Slušník
-
-document.ontouchmove = function(event){
+document.addEventListener('touchmove', function(event) {
     event.preventDefault();
-}
-export default function(el) {
-  el.addEventListener('touchstart', function() {
-    var top = el.scrollTop
-      , totalScroll = el.scrollHeight
-      , currentScroll = top + el.offsetHeight
+}, { passive: false });
 
-    //If we're at the top or the bottom of the containers
-    //scroll, push up or down one pixel.
-    //
-    //this prevents the scroll from "passing through" to
-    //the body.
-    if(top === 0) {
-      el.scrollTop = 1
-    } else if(currentScroll === totalScroll) {
-      el.scrollTop = top - 1
+document.body.addEventListener('touchstart', function(e){
+    var target = e.target;
+    if (!isScrollable(target)) {
+        e.preventDefault();
     }
-  })
+});
 
-  el.addEventListener('touchmove', function(evt) {
-    //if the content is actually scrollable, i.e. the content is long enough
-    //that scrolling can occur
-    if(el.offsetHeight < el.scrollHeight)
-      evt._isScroller = true
-  })
+function isScrollable(element) {
+    while (element !== document.body) {
+        const overflowY = window.getComputedStyle(element)['overflow-y'];
+        if (overflowY === 'scroll' || overflowY === 'auto') {
+            return true;
+        }
+        element = element.parentNode;
+    }
+    return false;
 }
-
-document.body.addEventListener('touchmove', function(evt) {
-  //In this case, the default behavior is scrolling the body, which
-  //would result in an overflow.  Since we don't want that, we preventDefault.
-  if(!evt._isScroller) {
-    evt.preventDefault()
-  }
-}, {passive: false})
 function submit() {
   document.getElementById("first").style.opacity = "0";
   setTimeout(function() {
