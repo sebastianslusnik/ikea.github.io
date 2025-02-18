d7024992bd63f8ede66e05f107e4a5c9cf2fa93e
Sebastián Slušník <46315566+sebastianslusnik@users.noreply.github.com>
2025-02-18 13:13:24 +0100

    Add files via upload

--- qr.js
+++ qr.js
@@ -1,3 +1,50 @@
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
+}
+
+async function setupCamera() {
+  const isPortrait = false; // do logic here
+
+  let video = document.getElementById('video');
+
+  console.log("Getting video");
+
+  video.setAttribute('autoplay', '');
+  video.setAttribute('muted', '');
+  video.setAttribute('playsinline', '');
+
+  console.log("Calling getUserMedia");
+
+  return new Promise((resolve) => {
+    (async() => {
+      await navigator.mediaDevices.getUserMedia({
+          'audio': false,
+          'video': {
+            facingMode: 'environment',
+          },
+        })
+        .then((stream) => {
+          console.log("Got getUserMedia stream");
+          video.srcObject = stream;
+          video.play();
+          resolve(true);
+        })
+        .catch((err) => {
+          console.log("Encountered getUserMedia error", err);
+          resolve(false);
+        });
+    })();
+  });
+
+}
+
+(async() => {
+  const ret = await setupCamera();
+  console.log(`Initialised camera: ${ret}`)
+})();
+
 function domReady(fn) {
     if (
         document.readyState === "complete" ||
