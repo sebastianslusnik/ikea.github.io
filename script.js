64707db635aeaa2ec98b8a5d316d70d5826be69c
Sebastián Slušník <46315566+sebastianslusnik@users.noreply.github.com>
2025-02-18 10:55:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -33,14 +33,38 @@ function submit() {
   }, 750);
 }
 function signout() {
-  document.getElementById("second").style.opacity = "0";
+  document.getElementById("third").style.opacity = "0";
   setTimeout(function() {
-    document.getElementById("second").style.display = "none";
+    document.getElementById("third").style.display = "none";
   }, 600);
   setTimeout(function() {
     document.getElementById("first").style.display = "flex";
   }, 650);
   setTimeout(function() {
     document.getElementById("first").style.opacity = "1";
+  }, 750);
+}
+function menu() {
+  document.getElementById("second").style.opacity = "0";
+  setTimeout(function() {
+    document.getElementById("second").style.display = "none";
+  }, 600);
+  setTimeout(function() {
+    document.getElementById("third").style.display = "flex";
+  }, 650);
+  setTimeout(function() {
+    document.getElementById("third").style.opacity = "1";
+  }, 750);
+}
+function menuBack() {
+  document.getElementById("third").style.opacity = "0";
+  setTimeout(function() {
+    document.getElementById("third").style.display = "none";
+  }, 600);
+  setTimeout(function() {
+    document.getElementById("second").style.display = "flex";
+  }, 650);
+  setTimeout(function() {
+    document.getElementById("second").style.opacity = "1";
   }, 750);
 }
