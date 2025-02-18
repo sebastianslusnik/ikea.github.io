f380e85d056bc1a8749dcded475222682733d001
Sebastián Slušník <46315566+sebastianslusnik@users.noreply.github.com>
2025-02-18 14:24:01 +0100

    Add files via upload

--- qr.js
+++ qr.js
@@ -1,50 +1,3 @@
-function isMobile() {
-  const isAndroid = /Android/i.test(navigator.userAgent);
-  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-  return isAndroid || isiOS;
-}
-
-async function setupCamera() {
-  const isPortrait = false; // do logic here
-
-  let video = document.getElementById('video');
-
-  console.log("Getting video");
-
-  video.setAttribute('autoplay', '');
-  video.setAttribute('muted', '');
-  video.setAttribute('playsinline', '');
-
-  console.log("Calling getUserMedia");
-
-  return new Promise((resolve) => {
-    (async() => {
-      await navigator.mediaDevices.getUserMedia({
-          'audio': false,
-          'video': {
-            facingMode: 'environment',
-          },
-        })
-        .then((stream) => {
-          console.log("Got getUserMedia stream");
-          video.srcObject = stream;
-          video.play();
-          resolve(true);
-        })
-        .catch((err) => {
-          console.log("Encountered getUserMedia error", err);
-          resolve(false);
-        });
-    })();
-  });
-
-}
-
-(async() => {
-  const ret = await setupCamera();
-  console.log(`Initialised camera: ${ret}`)
-})();
-
 function domReady(fn) {
     if (
         document.readyState === "complete" ||
@@ -79,4 +32,4 @@ setInterval(() => {
         selectElement.selectedIndex = 0;
         clearInterval(this);
     }
-}, 1000);
+}, 3000);
